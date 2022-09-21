221d1efc8476df339109fff9dc53ea10b3e40895
nunomuratoshiaki <58365651+nunomuratoshiaki@users.noreply.github.com>
2022-09-21 17:36:36 +0900

    Delete 11時間目 for文の応用 directory

--- 11時間目 for文の応用/script3.js
+++
@@ -1,6 +0,0 @@
-for(var a=3; a <= 8; a++){
-    if(a > 5){
-        document.write(a);
-        document.write("<br>");
-    }
-}
--- 11時間目 for文の応用/script7.js
+++
@@ -1,7 +0,0 @@
-for(var num=10; num >= 0; num--){
-    if(num == 0){
-        document.write("終了！" + "<br>");
-    }else{
-        document.write(num + "<br>");
-    }
-}
